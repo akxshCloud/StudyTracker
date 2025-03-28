d7b61a37c5d784f92c39977728f1358ec09b9837
Akash Unnikrishnan <a.unnikrishnan2@lse.ac.uk>
2025-03-28 11:47:03 +0000

    bug-fix: configuration of lottie animations corrected

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,27 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-defaultConfig.resolver.assetExts.push('lottie');
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  };
+  
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg').concat(['lottie']),
+    sourceExts: [...resolver.sourceExts, 'svg'],
+  };
+
+  return config;
+})();
 
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
- */
-module.exports = defaultConfig; 
+ */ 
